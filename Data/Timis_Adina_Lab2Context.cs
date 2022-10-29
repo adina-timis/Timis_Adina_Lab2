35edcb369f3e14d6c00bbcde034582327be07884
uSeR <uSeR@DESKTOP-J2OBVSE>
2022-10-29 13:18:48 +0300

    Add project files.

---
+++ Data/Timis_Adina_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Timis_Adina_Lab2.Models;
+
+namespace Timis_Adina_Lab2.Data
+{
+    public class Timis_Adina_Lab2Context : DbContext
+    {
+        public Timis_Adina_Lab2Context (DbContextOptions<Timis_Adina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Timis_Adina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Timis_Adina_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Timis_Adina_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,9 @@
+﻿namespace Timis_Adina_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
